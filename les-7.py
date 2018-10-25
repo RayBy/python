78e2310201264324c8f8e41e48d040888029d9eb
Ilinykh Pavel <ili.pas@outlook.com>
2018-10-25 17:19:13 +0200

    Доработал программу , теперь есть возможность выбирать координаты прорисовки

--- les-7.py
+++ les-7.py
@@ -4,6 +4,10 @@ import turtle
 import random
 import math
 
+PHI = 360/7
+R = 50
+
+
 def pengoto(x, y):
     turtle.penup()
     turtle.goto(x, y)
@@ -15,36 +19,48 @@ def circle_paint(r, color):
     turtle.circle(r)
     turtle.end_fill()
 
-turtle.speed(0)
+def baraban(base_x, base_y):
+    # Прорисовка основного круга
+    pengoto(base_x, base_y)
+    turtle.circle(80)
+    # Прорисовка мушки
+    pengoto(base_x, base_y+160)
+    circle_paint(5, "red")
+    # Прорисовка барабана
+    for i in range(0, 7):
+        phi_rad = PHI * i * math.pi / 180
+        pengoto(base_x+math.sin(phi_rad) * R, base_y+math.cos(phi_rad) * R + 60)
+        circle_paint(22, 'white')
 
-pengoto(0, 0)
-turtle.circle(80)
-pengoto(0, 160)
-circle_paint(5, "red")
+def roll(base_x, base_y, start):
+    for i in range(start, random.randrange(7, 100)):
+        phi_rad = PHI * i * math.pi / 180
+        pengoto(base_x+math.sin(phi_rad) * R, base_y+math.cos(phi_rad) * R + 60)
+        circle_paint(22, 'green')
+        circle_paint(22, 'white')
 
-phi = 360/7
-r = 50
+    pengoto(base_x+math.sin(phi_rad) * R, base_y+math.cos(phi_rad) * R + 60)
+    circle_paint(22, 'green')
 
-for i in range(0, 7):
-    phi_rad = phi*i*math.pi/180
-    pengoto(math.sin(phi_rad)*r, math.cos(phi_rad)*r+60)
-    turtle.circle(22)
-
-
-pengoto(math.sin(phi_rad)*r, math.cos(phi_rad)*r+60)
-circle_paint(22, "green")
-
+    return i % 7
 
 answer = ''
+start = 0
+turtle.speed(0)
+
+baraban(100, 100)
+
 while answer != "N":
-    answer = turtle.textinput("Нарисовать окружность?", "Y/N")
+    answer = turtle.textinput("Сыграем в русскую рулетку?", "Y/N")
+
     if answer == 'Y':
-        turtle.penup()
-        turtle.goto(random.randrange(1, 100), random.randrange(1, 100))
-        turtle.pendown()
-        turtle.fillcolor(random.random(), random.random(), random.random())
-        turtle.begin_fill()
-        turtle.circle(random.randrange(1, 250))
-        turtle.end_fill()
+       start = roll(100, 100, start)
+
+    if start == 0:
+        pengoto(-100, -150)
+        turtle.write("Вы проиграли!", font=("Arial", 18, "normal"))
     else:
-        pass
+        pengoto(-100, -150)
+        turtle.write("Вы победили!", font=("Arial", 18, "normal"))
+else:
+    pass
